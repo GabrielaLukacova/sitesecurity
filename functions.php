42f667dbd7aea19c154b1f192f3c9faafd2f17af
GabrielaLukacova <gabluk01@easv365.dk>
2023-11-24 12:37:38 +0100

    test push

--- functions.php
+++ functions.php
@@ -4,3 +4,24 @@ function sitesecurity_register_stylesheet() {
     wp_enqueue_style("sitesecurity-style", get_stylesheet_directory_uri() . "/style.css");
 }
 add_action("wp_enqueue_scripts", "sitesecurity_register_stylesheet");
+
+
+function theme_support() {
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+
+add_action('after_setup_theme', 'theme_support');
+
+function create_menu () {
+
+    $locations = array (
+        'primary' => "left sidebar",
+        'footer' => "footer menu items"
+    );
+    register_nav_menus($locations);
+}
+
+add_action('init', 'create_menu');
