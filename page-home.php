59ad315a157054231a657614cb2104e4b07c5ad3
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-01 20:55:50 +0100

    field
    field

--- page-home.php
+++ page-home.php
@@ -101,7 +101,7 @@ i
 
 <!--- Contact form section --->
 
-<section class="contact-form-container" style="background-image: url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 100vh;">
+<section class="contact-form-container" style="background-image: <?php $image = get_field('contact-form-image'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 100vh;">
     <h4 class="contact-form-heading"><?php the_field('contact_form_heading'); ?></h4>
     <div class="contact-form">
         <?php echo do_shortcode( '[contact-form-7 id="2e0ef46" title="Secure your business"]' ); ?>
