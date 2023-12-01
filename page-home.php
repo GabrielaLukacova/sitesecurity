92e85781786eb16f8d6c7616f3e352914387410b
Merrrrrrry <grs.merry@gmail.com>
2023-12-01 21:35:20 +0100

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -68,13 +68,11 @@ Template Name: Home
 <!--- Contact form section --->
 
 <section class="contact-form-container" style="background-image: <?php $image = get_field('contact-form-image'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 100vh;">
-<section class="contact-form-container" style="    font-size: 30px; padding: 10px 10% 0 0; margin: 0;">
     <h4 class="contact-form-heading" ><?php the_field('contact_form_heading'); ?></h4>
     <div class="contact-form">
         <?php echo do_shortcode( '[contact-form-7 id="2e0ef46" title="Secure your business"]' ); ?>
     </div>
 </section>
-
 
 
 
