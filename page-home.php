996bf3b3a748c07f90154632ecbf3183b7759627
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-01 22:20:58 +0100

    alaa

--- page-home.php
+++ page-home.php
@@ -103,40 +103,6 @@ Template Name: Home
  </section>
 
 
-<footer>
-        <div class="column links">
-            <a href="<?php echo get_permalink( get_page_by_path( 'home' ) ) ?>">Home</a>
-            <a href="<?php echo get_permalink( get_page_by_path( 'services' ) ) ?>">Services</a>
-            <a href="<?php echo get_permalink( get_page_by_path( 'faq' ) ) ?>">FAQ</a>
-            <a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ) ?>">Contact</a>
-        </div>
-
-        <div class="column image">
-        <a href="https://www.sitesecurity.dk/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/SiteSecurityLogo.png" alt="Logo" width="150" height="150">
-                </a>
-        </div>
-
-        <div class="column contact-info">
-                <div class= "footer_call_mail">
-                    <i class="material-icons">call</i>            
-                    <p> +45 88 80 84 48</p>
-            </div>
-                <div class= "footer_call_mail">
-                    <i class="material-icons">mail</i>   
-                    <p>danmark@sitesecurity.dk</p>
-            </div>
-            <div>
-                <a href="https://www.facebook.com/aktivovervaagning/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/facebook.png" alt="Facebook" width="50" height="50">
-                </a>
-
-                <a href="https://www.linkedin.com/company/sitesecurity/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" width="50" height="50">
-                </a>
-            </div>
-        </div>
-    </footer>
 
 </body>
 <?php endwhile ?>
