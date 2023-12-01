993d84113cf4b3561a6d70c5b52ef514f6bb63c0
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-01 21:09:23 +0100

    logo size

--- page-home.php
+++ page-home.php
@@ -147,7 +147,7 @@ i
 
         <div class="column image">
         <a href="https://www.sitesecurity.dk/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/SiteSecurityLogo.png" alt="Logo" width="200" height="200">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/images/SiteSecurityLogo.png" alt="Logo" width="250" height="250">
                 </a>
         </div>
 
