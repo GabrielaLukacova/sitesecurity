f7e63ff0e4de9b8647035d96b06ace66a0c346a1
Merrrrrrry <grs.merry@gmail.com>
2023-12-02 22:45:33 +0100

    s

--- page-contact.php
+++ page-contact.php
@@ -54,7 +54,7 @@ Template Name: Contact
 
 <section class="article_with_img__blue" style="background-image: <?php $image = get_field('article_image'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 320px; background-color: #AABFDF">
     <div class="article_with_img">
-        <div>
+        <div class="height_div_for_articles" >
             <p style="font-size: 40px; margin: 10px;"><?php the_field('article_title'); ?></p>   
             <p style="font-size: 24px; margin: 10px"><?php the_field('article_text'); ?></p>   
         </div>
@@ -85,7 +85,7 @@ Template Name: Contact
 
 <section id="article_with_image_call_us" style="background-image: <?php $image = get_field('call_hours_image'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: auto; height: 500px; background-color: #F6F6F6;">
     <div class="article_with_img">
-        <div class="call_us_article" > 
+        <div class="height_div_for_articles call_us_article " > 
             <p class="optima" style="font-size: 40px; margin: 10px;"><?php the_field('call_hours_heading'); ?></p>   
             <p style="font-size: 24px; margin: 10px"><?php the_field('call_hours_description'); ?></p>   
                 <div class="optima" style="font-size: 30px; display: flex; align-items: center; gap: 10px;">
@@ -95,7 +95,7 @@ Template Name: Contact
             <p class="optima" style="font-size: 40px; margin: 10px;"><?php the_field('call_hours'); ?></p>   
         </div>
     </div>
-</section>
+</section> 
 
 
 
@@ -110,7 +110,7 @@ Template Name: Contact
 
 <section id="article_with_image_yellow" style="background-image: <?php $image = get_field('other_businesses_secure_img'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 320px; background-color: #FCD53F;">
     <div class="article_with_img">
-        <div> 
+        <div class="height_div_for_articles"> 
             <p style="text-align: right; font-size: 40px; margin: 10px;">
              <?php the_field('other_businesses_secure_heading'); ?></p>   
             <p style="text-align: right; font-size: 24px; margin: 10px">
