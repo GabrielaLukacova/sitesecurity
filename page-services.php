877ead0240b19d8d1a8af7ae256cff1704f85f83
Merrrrrrry <grs.merry@gmail.com>
2023-12-03 00:26:58 +0100

    style="talign-self: center;">

--- page-services.php
+++ page-services.php
@@ -66,8 +66,8 @@ Template Name: Services
                         <h3 class="heading_white" style=" background-color: rgba(51, 51, 51, 0.7); font-size: 18px; padding: 10px 10px;"><?php echo get_field("product_heading"); ?></h3>
                         <p class=" article-text heading_white" style=" text-align: right; background-color: rgba(51, 51, 51, 0.7); color: #fff; font-size: 18px; margin: 0 auto; padding: 10px 10px;">
                         <?php echo get_field("product_description"); ?></p>
-                        <a href="<?php echo get_permalink( get_page_by_path( 'services' ) ) ?>" class="button_yellow">Read more</a>
                     </div>
+                    <a href="<?php echo get_permalink( get_page_by_path( 'services' ) ) ?>" class="button_yellow" style="talign-self: center;">Read more</a>
                 </div>
             </div>
         <?php endwhile; wp_reset_query(); ?>
