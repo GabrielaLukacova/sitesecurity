c8a16e7ae2fea3d2037353079640424383db2784
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-03 03:06:58 +0100

    a

--- script.js
+++ script.js
@@ -22,20 +22,25 @@ jQuery(document).ready(function($) {
     });
 
     // FAQ - opening box after clicking on arrow
-    $('.faq-container_box .question').on('click', function () {
-        const answer = $(this).next();
-
-        // Toggle the 'open' class to trigger CSS transitions
-        answer.toggleClass('open');
-
-        // Toggle the arrow direction
-        const arrow = $(this).find('.arrow');
-        arrow.text(arrow.text() === '▶' ? '▼' : '▶');
+    document.addEventListener('DOMContentLoaded', function () {
+        const questions = document.querySelectorAll('.faq-container_box .question');
+    
+        questions.forEach(question => {
+            question.addEventListener('click', function () {
+                const answer = this.nextElementSibling;
+    
+                // Toggle the 'open' class to trigger CSS transitions
+                answer.classList.toggle('open');
+    
+                // Toggle the arrow direction
+                const arrow = this.querySelector('.arrow');
+                arrow.textContent = arrow.textContent === '▶' ? '▼' : '▶';
+            });
+        });
+    
+        console.log('Script.js is loaded!');
     });
     
-    console.log('Script.js is loaded!');
-});
-
 
 
 /*document.addEventListener('DOMContentLoaded', function () {
