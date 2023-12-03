305607ed80037686a94885baec40c2f156975a98
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-03 04:57:37 +0100

    a

--- page-faq.php
+++ page-faq.php
@@ -10,23 +10,25 @@ Template Name: FAQ
     <?php while (have_posts()) : the_post(); ?>
         <?php the_content(); ?>
 
-        <!-- Hero Section with navbar -->
-        <div class="hero-section" style="background-image: url('<?php echo esc_url(get_field('hero_video')['url']); ?>'); background-size: cover; background-position: center; height: 100vh;">
-            <div class="navbar">
-                <a class="home" href="<?php echo esc_url(get_permalink(get_page_by_path('home'))); ?>" style="order: -1;">Home</a>
-                <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>">Contacts</a>
-                <a href="<?php echo esc_url(get_permalink(get_page_by_path('faq'))); ?>">FAQ</a>
-                <a href="<?php echo esc_url(get_permalink(get_page_by_path('services'))); ?>">Services</a>
-            </div>
-        </div>
-        
-        <div class="hero-content">
-            <h1 class="hero-text"><?php the_field('hero_text'); ?></h1>
-            <p class="hero-slogan"><?php the_field('hero_slogan'); ?></p>
+       <!-- Hero Section with navbar -->
+
+<div class="hero-section"   style="background-image: <?php $image = get_field('hero_video'); ?> url('<?php echo esc_url($image["url"]); ?>'); background-size: cover; background-position: center; height: 100vh;">
+        <div class="navbar">
+            <a class="home" href="<?php echo get_permalink(get_page_by_path('home')) ?>" style="order: -1;">Home</a>
+            <a href="<?php echo get_permalink(get_page_by_path('services')) ?>">Services</a>
+            <a href="<?php echo get_permalink(get_page_by_path('faq')) ?>">FAQ</a>
+            <a href="<?php echo get_permalink(get_page_by_path('contact')) ?>">Contacts</a>
         </div>
 
+    </div>
+    <div class="hero-content">
+        <h1 class="hero-text"><?php the_field('hero_text'); ?></h1>
+        <p class="hero-slogan"><?php the_field('hero_slogan'); ?></p>
+    </div>
+</div>
+
         <!-- FAQ Section -->
-       <!-- page-faq.php -->
+       
 <section class="faq">
     <h2 class="heading-above"><?php echo get_field("fqa_main_article_title"); ?></h2>
     <div class="faq_container">
