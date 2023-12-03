cd002dc25bcbdd54b3b95857fce0b82ce197bc11
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-03 04:38:26 +0100

    vanilla js

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-jQuery(document).ready(function($) {
+document.addEventListener('DOMContentLoaded', function () {
     // SLIDE SHOW - customers reviews
-    $('.slick-carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        fade: true,
-        cssEase: 'linear',
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false
+    document.querySelectorAll('.slick-carousel').forEach(function (carousel) {
+        new Glider(carousel, {
+            dots: '#dots',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            draggable: true,
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+            },
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false
+                    }
                 }
-            }
-        ]
+            ]
+        });
     });
 
     // FAQ - opening box after clicking on arrow
-    $('.faq-container_box .question').on('click', function () {
-        console.log('Click event triggered.');
+    document.querySelectorAll('.faq-container_box .question').forEach(function (question) {
+        question.addEventListener('click', function () {
+            console.log('Click event triggered.');
 
-        const answer = $(this).closest('.faq-container_box').find('.answer');
+            var answer = this.closest('.faq-container_box').querySelector('.answer');
 
-        // Toggle the 'open' class to trigger CSS transitions
-        answer.toggleClass('open');
+            // Toggle the 'open' class to trigger CSS transitions
+            answer.classList.toggle('open');
 
-        // Toggle the arrow direction
-        const arrow = $(this).find('.arrow');
-        arrow.text(arrow.text() === '▶' ? '▼' : '▶');
+            // Toggle the arrow direction
+            var arrow = this.querySelector('.arrow');
+            arrow.textContent = arrow.textContent === '▶' ? '▼' : '▶';
+        });
     });
 });
