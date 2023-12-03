ba4ebb07bbd46a72094f82933acc0db20556e3c1
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-12-03 22:31:58 +0100

    deleted script lines

--- functions.php
+++ functions.php
@@ -4,10 +4,6 @@ function sitesecurity_register_stylesheet() {
     wp_enqueue_style("sitesecurity-style", get_stylesheet_directory_uri() . "/style.css");
     wp_enqueue_style("materialize-icons", "https://fonts.googleapis.com/icon?family=Material+Icons");
     wp_enqueue_style("din_condensed_font", "https://use.typekit.net/piy2rgn.css");
-    wp_enqueue_style("slick", "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css");
-    wp_enqueue_style("slick-caro", "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css");
-    wp_enqueue_script("slick", "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js", array('jquery'), null, true);
-    wp_enqueue_script("script", get_stylesheet_directory_uri() . "/script.js", array('jquery'), null, true);
 }
 
 add_action("wp_enqueue_scripts", "sitesecurity_register_stylesheet");
